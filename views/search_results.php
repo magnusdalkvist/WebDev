019b6f990a39ac0df5e23523450ea779eb0be806
lass417r <lass417r@stud.kea.dk>
2023-11-23 17:20:34 +0100

    search works

--- views/search_results.php
+++ views/search_results.php
@@ -1,30 +1,30 @@
 <?php
-require_once __DIR__.'/_header.php';
+require_once __DIR__ . '/_header.php';
 
 $db = _db();
 $q = $db->prepare(' SELECT * FROM users 
-                    WHERE user_name = :word COLLATE NOCASE 
-                    OR user_last_name = :word COLLATE NOCASE');
-                    
-$q->bindValue(':word', $_GET['query']);
+                    WHERE user_name LIKE :word 
+                    OR user_last_name LIKE :word');
+$q->bindValue(':word', '%' . $_GET['query'] . '%');
 $q->execute();
 $users = $q->fetchAll();
-
 ?>
 
+
+
 <main class="px-4">
-<?php foreach($users as $user): ?>
+  <?php foreach ($users as $user) : ?>
     <div class="flex">
       <div class="hidden"><?= $user['user_id'] ?></div>
       <div class="w-1/5"><?php out($user['user_name']) ?></div>
       <div class="w-1/5"><?php out($user['user_last_name']) ?></div>
       <div class="w-1/5"><?php out($user['user_email']) ?></div>
-      <div class="w-1/5"><?php out($user['user_role_name']) ?></div>
+      <div class="w-1/5"><?php out($user['user_role']) ?></div>
       <button class="w-1/5">🗑️</button>
     </div>
   <?php endforeach ?>
 </main>
 
 <?php
-require_once __DIR__.'/_footer.php';
+require_once __DIR__ . '/_footer.php';
 ?>

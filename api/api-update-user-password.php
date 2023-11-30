71ccb55fcbc6f6f3eaefe8daf3f361f8b7e21845
lass417r <lass417r@stud.kea.dk>
2023-12-01 00:48:37 +0100

    update user password

--- api/api-update-user-password.php
+++ api/api-update-user-password.php
@@ -3,34 +3,11 @@ require_once __DIR__ . '/../_.php';
 
 try {
 
-    if (!isset($_SESSION['user_id'])) {
-        throw new Exception('User not logged in', 401);
-    }
-
-    $user = $_SESSION['user'];
-
-    if (!password_verify($_POST['user_old_password'], $user['user_password'])) {
-        throw new Exception('Invalid old password', 400);
-    }
 
 
-    $db = _db();
-    $q = $db->prepare('UPDATE users SET  user_password = :user_password, user_updated_at = :user_updated_at WHERE user_id = :user_id');
-
-    $q->bindValue(':user_updated_at', time());
-    $q->bindValue(':user_password', $_POST['user_password'] ? password_hash($_POST['user_password'], PASSWORD_DEFAULT) : $user['user_password']);
-
-    $q->execute();
-
-    if ($q->rowCount() == 0) {
-        throw new Exception('No rows were updated');
-    }
 
 
-    // Update the $_SESSION['user'] variable with the latest user information
-    $_SESSION['user'] = $user;
-
-    echo json_encode(['message' => 'User updated successfully']);
+    echo json_encode(['message' => 'User password updated successfully']);
 } catch (Exception $e) {
     try {
         if (!$e->getCode() || !$e->getMessage()) {

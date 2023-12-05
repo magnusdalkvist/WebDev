90d8c01e429fb5588461b8283ef8589e8288ae8e
lass417r <lass417r@stud.kea.dk>
2023-12-05 12:51:50 +0100

    account.php password form

--- views/account.php
+++ views/account.php
@@ -19,8 +19,8 @@ $q->execute();
 $orders = $q->fetchAll();
 ?>
 
-<main class="grid grid-cols-2 gap-4">
-  <section class="flex flex-col">
+<section class="grid grid-cols-2 gap-4">
+  <div class="flex flex-col">
     <div class="flex  gap-2 flex-col mt-16 p-8 mr-4 bg-white rounded-md text-slate-500">
       <h2 class="font-extrabold ">Profile</h2>
 
@@ -92,8 +92,8 @@ $orders = $q->fetchAll();
       </form>
     </div>
 
-  </section>
-  <section class="flex flex-col ">
+  </div>
+  <div class="flex flex-col ">
     <div id="update_account" class="flex flex-col mt-16 p-8 mr-4  bg-white rounded-md text-slate-500">
       <div class="pb-4">
         <h2 class="font-extrabold ">Update profile</h2>
@@ -140,8 +140,8 @@ $orders = $q->fetchAll();
         <input type="submit" value="Update">
       </form>
     </div>
-  </section>
-</main>
+  </div>
+</section>
 
 <?php
 require_once __DIR__ . '/_footer.php';

6db426a191f7f060f5f306aa8709fecbad0eb60e
Magnus Dalkvist <magndut@gmail.com>
2023-12-06 18:09:36 +0100

    Styled more

--- views/login.php
+++ views/login.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/_header.php';
 ?>
 
 <section class="flex flex-col items-center">
-  <div class="flex flex-col mt-20 py-8 px-8 bg-white rounded-md text-slate-500" style="width: 33%;">
+  <div class="flex flex-col mt-20 py-8 px-8 bg-50-shades rounded-md text-soft-white w-1/3">
     <h1>Welcome back</h1>
     <p>Login with email</p>
     <form onsubmit="validate(login); return false" class="flex flex-col gap-6 w-full h-full m-auto pt-8">
@@ -16,10 +16,10 @@ require_once __DIR__ . '/_header.php';
         <input class="pl-2 bg-transparent placeholder:text-transparent-50 focus:outline-none" name="user_password" type="password" id="password" placeholder="Password" data-validate="str" data-min="<?= USER_PASSWORD_MIN ?>" data-max="<?= USER_PASSWORD_MAX ?>">
       </label>
       <div class="text-sexyred" id="login_error"></div>
-      <button class="w-full h-10 my-1 bg-sexyred text-white font-bold rounded-md">Login</button>
+      <button class="w-full px-4 py-3 bg-sexyred text-white font-bold rounded-2xl">Login</button>
     </form>
   </div>
-  <div class="text-sexyred mt-8 font-bold flex justify-center "><a href="/signup">Or signup</a></div>
+  <div class="text-sexyred mt-4 font-bold flex justify-center "><a href="/signup">Or signup</a></div>
 </section>
 
 <?php
